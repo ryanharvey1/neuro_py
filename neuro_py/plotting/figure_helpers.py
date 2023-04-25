29e7da8d4ae2d73f2b1ecaf47bc9435f3e220763
Ryan Harvey <ryanh412@gmail.com>
2023-04-25 14:34:22 -0400

    adding more figure helpers

--- neuro_py/plotting/figure_helpers.py
+++ neuro_py/plotting/figure_helpers.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import PathPatch
 
 def set_plotting_defaults():
     tex_fonts = {
@@ -54,3 +56,88 @@ def set_size(width, fraction=1, subplots=(1, 1)):
     fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
 
     return (fig_width_in, fig_height_in)
+
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens a color by a certain percentage.
+    This is useful for adjusting colors for a particular element of a page.
+    :param color: The hex color code, e.g. #AABBCC
+    :param amount: The amount to lighten the color by.
+    :return: The lightened color code in hex, e.g. #FFFFFF
+    """
+    try:
+        c = color.lstrip("#")
+        c = tuple(int(c[i : i + 2], 16) for i in (0, 2, 4))
+        c = (
+            int((1 - amount) * c[0] + amount * 255),
+            int((1 - amount) * c[1] + amount * 255),
+            int((1 - amount) * c[2] + amount * 255),
+        )
+        return "#%02x%02x%02x" % c
+    except ValueError:
+        return color
+    
+
+def set_equal_axis_range(ax1, ax2):
+    """
+    Makes x and y min and max the same between two plots
+    """
+    axis_x_values = np.hstack(np.array((ax1.get_xlim(), ax2.get_xlim())))
+    axis_y_values = np.hstack(np.array((ax1.get_ylim(), ax2.get_ylim())))
+    ax1.set_xlim(axis_x_values.min(), axis_x_values.max())
+    ax1.set_ylim(axis_y_values.min(), axis_y_values.max())
+    ax2.set_xlim(axis_x_values.min(), axis_x_values.max())
+    ax2.set_ylim(axis_y_values.min(), axis_y_values.max())
+
+
+def restore_natural_scale(ax, min_, max_, n_steps=4, x_axis=True, y_axis=True):
+    """
+    takes x and y ax that are in log10 and puts them into natural scale
+
+    By default, it adjusts both x and y, but you can run this on a single
+    axis or two times if you have different scales for x and y
+    """
+    ticks = np.linspace(min_, max_, n_steps)
+
+    if x_axis:
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(np.round(10**ticks, 3))
+
+    if y_axis:
+        ax.set_yticks(ticks)
+        ax.set_yticklabels(np.round(10**ticks, 3))
+
+
+def adjust_box_widths(g, fac):
+    """
+    Adjust the widths of a seaborn-generated boxplot.
+    """
+
+    # iterating through Axes instances
+    for ax in g.axes:
+
+        # iterating through axes artists:
+        for c in ax.get_children():
+
+            # searching for PathPatches
+            if isinstance(c, PathPatch):
+                # getting current width of box:
+                p = c.get_path()
+                verts = p.vertices
+                verts_sub = verts[:-1]
+                xmin = np.min(verts_sub[:, 0])
+                xmax = np.max(verts_sub[:, 0])
+                xmid = 0.5 * (xmin + xmax)
+                xhalf = 0.5 * (xmax - xmin)
+
+                # setting new width of box
+                xmin_new = xmid - fac * xhalf
+                xmax_new = xmid + fac * xhalf
+                verts_sub[verts_sub[:, 0] == xmin, 0] = xmin_new
+                verts_sub[verts_sub[:, 0] == xmax, 0] = xmax_new
+
+                # setting new width of median line
+                for l in ax.lines:
+                    if np.all(l.get_xdata() == [xmin, xmax]):
+                        l.set_xdata([xmin_new, xmax_new])
