7bd14fd29e4954dd2ac347f48f5dca8f0b4a0e36
Ryan Harvey <ryanh412@gmail.com>
2025-02-23 18:12:26 -0500

    Revert "add lazy init"
    This reverts commit 7ad33ad4b3ed6fd91e45fd85cf12201f63263007.

--- neuro_py/ensemble/decoding/__init__.py
+++ neuro_py/ensemble/decoding/__init__.py
@@ -1,4 +1,47 @@
-import lazy_loader as _lazy
+__all__ = [
+    "MLP",
+    "LSTM",
+    "M2MLSTM",
+    "NDT",
+    "seed_worker",
+    "get_spikes_with_history",
+    "format_trial_segs_nsv",
+    "zscore_trial_segs",
+    "normalize_format_trial_segs",
+    "minibatchify",
+    "normalize_labels",
+    "create_model",
+    "preprocess_data",
+    "evaluate_model",
+    "shuffle_nsv_intrialsegs",
+    "train_model",
+    "predict_models_folds",
+    "split_data",
+    "partition_indices",
+    "partition_sets",
+]
 
-(__getattr__, __dir__, __all__) = _lazy.attach_stub(__name__, __file__)
-del _lazy
+from .mlp import MLP
+from .lstm import LSTM
+from .m2mlstm import M2MLSTM
+from .transformer import NDT
+from .pipeline import (
+    seed_worker,
+    get_spikes_with_history,
+    format_trial_segs_nsv,
+    zscore_trial_segs,
+    normalize_format_trial_segs,
+    minibatchify,
+    normalize_labels,
+    create_model,
+    preprocess_data,
+    evaluate_model,
+    shuffle_nsv_intrialsegs,
+    train_model,
+    predict_models_folds,
+)
+from .preprocess import (
+    split_data,
+    partition_indices,
+    partition_sets,
+)
