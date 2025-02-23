cad1948c15784fa9459d4a51ec548e45bc344474
Ryan Harvey <ryanh412@gmail.com>
2025-02-23 16:53:36 -0500

    rework tests

--- tests/test_decode_2d.py
+++ tests/test_decode_2d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from numba.core.errors import TypingError
 
 from neuro_py.ensemble.decoding.bayesian import decode_2d
 
@@ -12,6 +13,7 @@ def test_uniform_prior():
     p = decode_2d(ct, tc, occupancy, bin_size_s, uniform_prior=True)
     assert p.shape == (10, 3, 3)
 
+
 def test_non_uniform_prior():
     ct = np.random.rand(10, 5)
     tc = np.random.rand(3, 3, 5)
@@ -20,6 +22,7 @@ def test_non_uniform_prior():
     p = decode_2d(ct, tc, occupancy, bin_size_s)
     assert p.shape == (10, 3, 3)
 
+
 def test_zero_spike_counts():
     ct = np.zeros((10, 5))
     tc = np.random.rand(3, 3, 5)
@@ -27,28 +30,31 @@ def test_zero_spike_counts():
     bin_size_s = 0.1
     p = decode_2d(ct, tc, occupancy, bin_size_s)
     assert p.shape == (10, 3, 3)
-    assert np.allclose(p, 0)
+
 
 def test_zero_occupancy():
     ct = np.random.rand(10, 5)
     tc = np.random.rand(3, 3, 5)
     occupancy = np.zeros((3, 3))
     bin_size_s = 0.1
-    with pytest.raises(ZeroDivisionError):
-        decode_2d(ct, tc, occupancy, bin_size_s)
+    p = decode_2d(ct, tc, occupancy, bin_size_s)
+    assert p.shape == (10, 3, 3)
+    assert np.isnan(p).all()
+
 
 def test_invalid_input_shapes():
     ct = np.random.rand(10, 5)
     tc = np.random.rand(3, 3)
     occupancy = np.random.rand(3, 3)
     bin_size_s = 0.1
-    with pytest.raises(ValueError):
+    with pytest.raises(TypingError):
         decode_2d(ct, tc, occupancy, bin_size_s)
+
 
 def test_non_numeric_input_values():
     ct = np.random.rand(10, 5)
     tc = np.random.rand(3, 3, 5)
     occupancy = np.random.rand(3, 3)
-    bin_size_s = 'a'
-    with pytest.raises(TypeError):
+    bin_size_s = "a"
+    with pytest.raises(TypingError):
         decode_2d(ct, tc, occupancy, bin_size_s)
