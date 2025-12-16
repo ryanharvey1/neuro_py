21c659cd240055d41760a81de3ecf533e9d2394e
Ryan Harvey <ryanh412@gmail.com>
2025-12-16 11:44:36 -0500

    add to init

--- neuro_py/detectors/__init__.pyi
+++ neuro_py/detectors/__init__.pyi
@@ -1,4 +1,15 @@
-__all__ = ["DetectDS", "detect_up_down_states"]
+__all__ = [
+    "DetectDS",
+    "detect_up_down_states",
+    "detect_up_down_states_bimodal_thresh",
+    "hartigan_diptest",
+    "bimodal_thresh",
+]
 
 from .dentate_spike import DetectDS
-from .up_down_state import detect_up_down_states
+from .up_down_state import (
+    bimodal_thresh,
+    detect_up_down_states,
+    detect_up_down_states_bimodal_thresh,
+    hartigan_diptest,
+)
