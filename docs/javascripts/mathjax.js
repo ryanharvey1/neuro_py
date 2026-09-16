1180ae416a13f7c608413d206967a4e61451571a
Ryan Harvey <ryanh412@gmail.com>
2026-09-16 16:08:25 -0400

    Render nested tutorial math (#164)

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,13 +1,12 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
     processEscapes: true,
     processEnvironments: true,
   },
   options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex",
+    ignoreHtmlClass: "no-mathjax",
   },
 };
 
